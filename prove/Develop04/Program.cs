8bee26c32fdd90b05a35e6a5c1b68fa0551b69c0
laurelcerrato <laurelcerrato@gmail.com>
2023-02-21 20:25:29 -0600

    Created classes

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -3,7 +3,20 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Develop04 World!");
+    {   Console.Clear();
+        string choice = "";
+        while (choice != "4"){
+            Console.WriteLine("Menu Options:\n1. Start breathing activity\n2. Start reflecting activity\n3. Start listing activity\n4. Quit");
+            Console.Write("Select a choice from the menu: ");
+            choice = Console.ReadLine();
+            if (choice == "1"){
+                Console.WriteLine("Going to sleep for a second...");
+                Thread.Sleep(5000);
+            Console.WriteLine("I'm back!!");
+            }else if (choice == "2"){
+                Activity ac = new Activity();
+                Console.Write("Choice 2 ");ac.Countdown();
+            }
+        }
     }
-}
+}       
