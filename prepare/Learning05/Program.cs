dadb2f201efcbe20068235b3abe5f0ac3778a9fa
laurelcerrato <laurelcerrato@gmail.com>
2023-03-07 20:54:20 -0600

    learning activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        Square s = new Square("Green", 13);
+        shapes.Add(s);
+        Rectangle r = new Rectangle("Blue", 13,10);
+        shapes.Add(r);
+        Circle c = new Circle("Yellow", 20);
+        shapes.Add(c);
+
+        foreach(Shape sh in shapes){
+            Console.WriteLine(sh.GetColor());
+            Console.WriteLine(sh.GetArea());
+        }
     }
 }
