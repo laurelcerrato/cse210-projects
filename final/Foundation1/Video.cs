8078e7c7e98546ec81ecb9c79c470b405ebc6f85
laurelcerrato <laurelcerrato@gmail.com>
2023-03-25 22:28:14 -0600

    added attributes and methods to the program 1

---
+++ final/Foundation1/Video.cs
@@ -0,0 +1,14 @@
+public class Video{
+    public string _title;
+    public string _author;
+
+    public int _lenght;
+    public List<string> _comments;
+
+    public int Amount(List<string>){
+
+    }
+    public void DisplayVideoInfo(){
+        
+    }
+}
