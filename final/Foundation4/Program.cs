198d73fe5b95146c4f6092b5360633fd5fc1b187
laurelcerrato <laurelcerrato@gmail.com>
2023-03-29 22:09:49 -0600

    detail

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -8,7 +8,7 @@ class Program
         List<Activity> activities = new List<Activity>();
         Running running = new Running("09-03/2023",20,"Running",20);
         Cycling cycling = new Cycling("08/04/2023",30,"Cycling",7);
-        Swimming swimming = new Swimming("05/03/2023",20,"Swimming",10);
+        Swimming swimming = new Swimming("05/03/2023",20,"Swimming",20);
         activities.Add(running);
         activities.Add(cycling);
         activities.Add(swimming);
